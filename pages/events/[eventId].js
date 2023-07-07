8bdd83ce18ac43a52903f85b8a6301e3301232fe
Malgorzata Jaskiewicz <m.jaskiewicz@connexin.co.uk>
2023-07-07 20:02:32 +0100

    repair structure

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,15 +1,16 @@
 import { useRouter } from 'next/router';
-import { getEventById } from '../../dummy-data';
 import { Fragment } from 'react';
-import EventSummary from '../../components/event-detail/event-summary';
+
 import EventContent from '../../components/event-detail/event-content';
 import EventLogistics from '../../components/event-detail/event-logistics';
+import EventSummary from '../../components/event-detail/event-summary';
+import { getEventById } from '../../dummy-data';
 
-function EventDetalPage() {
+function EventDetailPage() {
   const router = useRouter();
 
-  const eventID = router.query.eventID;
-  const event = getEventById(eventID);
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
 
   if (!event) {
     return <p>No event found!</p>;
@@ -20,7 +21,7 @@ function EventDetalPage() {
       <EventSummary title={event.title} />
       <EventLogistics
         date={event.date}
-        address={event.address}
+        address={event.location}
         image={event.image}
         imageAlt={event.title}
       />
@@ -31,4 +32,4 @@ function EventDetalPage() {
   );
 }
 
-export default EventDetalPage;
+export default EventDetailPage;
